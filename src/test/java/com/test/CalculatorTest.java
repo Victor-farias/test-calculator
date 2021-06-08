51090fb4ea0fa7bc01d115436593ef8a5f9ce869
Victor-farias <fariasvictor449@gmail.com>
2021-06-08 15:25:17 -0300

    calculator sum test with mock

--- src/test/java/com/test/CalculatorTest.java
+++ src/test/java/com/test/CalculatorTest.java
@@ -3,6 +3,8 @@ package com.test;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class CalculatorTest {
 
@@ -12,5 +14,16 @@ public class CalculatorTest {
         int sum = calculator.sum("1+1+3");
 
         assertEquals(5, sum);
+    }
+
+    @Test
+    public void sumTestWithMock() {
+        Calculator calculator = mock(Calculator.class);
+
+        when(calculator.sum("1 + 2")).thenReturn(10);
+
+        int result = calculator.sum("1 + 2");
+
+        assertEquals(10, result);
     }
 }
