35a44b98ac52dd057cd2e4e701a064c7d542d19a
Victor-farias <fariasvictor449@gmail.com>
2021-06-08 15:56:54 -0300

    Matcher test

---
+++ src/test/java/com/test/MatcherTest.java
@@ -0,0 +1,30 @@
+package com.test;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+public class MatcherTest {
+
+    @Test
+    public void givenCollection_chekIfEmpty() {
+        List<String> emptyList = new ArrayList<String>();
+        assertThat(emptyList, Matchers.empty());
+    }
+
+    @Test
+    public void givenAnInteger_checkGreater() {
+        assertThat(1, Matchers.greaterThan(0));
+    }
+
+    public void givenString_checkIsNull() {
+        String str = null;
+        assertThat(str, Matchers.isEmptyOrNullString());
+    }
+
+}
